050526ba4ea384da64849c2e6ab426b37b003aa1
Ali <alifaki077@gmail.com>
2024-12-23 23:51:11 +0300

    Day 22 part 2

--- Day22/Program.cs
+++ Day22/Program.cs
@@ -1,19 +1,39 @@
 ﻿string inputFile = "input.txt";
 // inputFile = "test.txt";
 
-long[] secrets = File.ReadLines(inputFile).Select(long.Parse).ToArray();
-
 int iters = 2000;
 
-for (int i = 0; i < iters; i++)
+long[] secrets = File.ReadLines(inputFile).Select(long.Parse).ToArray();
+Dictionary<(long, long, long, long), long> seqs = new();
+
+for (int i = 0; i < secrets.Length; i++)
 {
-    for (int j = 0; j < secrets.Length; j++)
+    long s1 = 0, s2 = 0, s3 = 0, s4 = 0;
+    HashSet<(long, long, long, long)> firstApperance = new();
+    for (int j = 0; j < iters; j++)
     {
-        secrets[j] = PRNG(secrets[j]);
+        long prevPrice = secrets[i] % 10;
+        secrets[i] = PRNG(secrets[i]);
+        long newPrice = secrets[i] % 10;
+
+        s1 = s2;
+        s2 = s3;
+        s3 = s4;
+        s4 = newPrice - prevPrice;
+
+        if (j >= 3 && !firstApperance.Contains((s1, s2, s3, s4)))
+        {
+            if (!seqs.ContainsKey((s1, s2, s3, s4)))
+                seqs[(s1, s2, s3, s4)] = 0;
+            seqs[(s1, s2, s3, s4)] += newPrice;
+            firstApperance.Add((s1, s2, s3, s4));
+        }
     }
 }
 
 Console.WriteLine($"Part 1: {secrets.Sum()}");
+
+Console.WriteLine($"Part 2: {seqs.Max(kvp => kvp.Value)}");
 
 long PRNG(long secret)
 {
